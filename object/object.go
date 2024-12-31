961479a21528c9c71822536016177f1cb00fdd1f
hyuraku <32809703+hyuraku@users.noreply.github.com>
2024-12-31 21:10:58 +0900

    add: implement Boolean and Null object types with corresponding methods

--- object/object.go
+++ object/object.go
@@ -4,6 +4,8 @@ import "fmt"
 
 const (
 	INTEGER_OBJ = "INTEGER"
+	BOOLEAN_OBJ = "BOOLEAN"
+	NULL_OBJ    = "NULL"
 )
 
 type ObjectType string
@@ -19,3 +21,14 @@ type Integer struct {
 
 func (i *Integer) Type() ObjectType { return INTEGER_OBJ }
 func (i *Integer) Inspect() string { return fmt.Sprintf("%d", i.Value) }
+
+type Boolean struct {
+	Value bool
+}
+
+func (b *Boolean) Type() ObjectType { return BOOLEAN_OBJ }
+func (b *Boolean) Inspect() string { return fmt.Sprintf("%t", b.Value) }
+
+type Null struct{}
+func (n *Null) Type() ObjectType { return NULL_OBJ }
+func (n *Null) Inspect() string { return "null" }

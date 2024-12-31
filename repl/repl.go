c96df5e413be4c846b4026277f275adc5d0386f0
hyuraku <32809703+hyuraku@users.noreply.github.com>
2024-12-31 23:32:11 +0900

    add: integrate evaluator for program execution in REPL

--- repl/repl.go
+++ repl/repl.go
@@ -4,8 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"monkey/evaluator"
 	"monkey/lexer"
-	// "monkey/token"
 	"monkey/parser"
 )
 
@@ -37,8 +37,13 @@ func Start(in io.Reader, out io.Writer){
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
+		// io.WriteString(out, program.String())
+		// io.WriteString(out, "\n")
 		
 	}
 }

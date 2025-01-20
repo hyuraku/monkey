1510f37a0e663c1bd1370e4423707a9205b73504
hyuraku <32809703+hyuraku@users.noreply.github.com>
2025-01-20 21:32:02 +0900

    add: implement recursive compilation for program and expression statements

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -19,6 +19,32 @@ func New() *Compiler {
 }
 
 func (c *Compiler) Compile(node ast.Node) error {
+	switch node := node.(type) {
+	case *ast.Program:
+		for _, statement := range node.Statements {
+			err := c.Compile(statement)
+			if err != nil {
+				return err
+			}
+		}
+	case *ast.ExpressionStatement:
+		err := c.Compile(node.Expression)
+		if err != nil {
+			return err
+		}
+	case *ast.InfixExpression:
+		err := c.Compile(node.Left)
+		if err != nil {
+			return err
+		}
+		err = c.Compile(node.Right)
+		if err != nil {
+			return err
+		}
+	case *ast.IntegerLiteral:
+		integer := &object.Integer{Value: node.Value}
+		c.emit(code.OpConstant, c.addConstant(integer))
+	}
 	return nil
 }
 
@@ -32,4 +58,21 @@ func (c *Compiler) Bytecode() *Bytecode {
 		Instructions: c.instructions,
 		Constants:    c.constants,
 	}
+}
+
+func (c *Compiler) addConstant(obj object.Object) int {
+	c.constants = append(c.constants, obj)
+	return len(c.constants) - 1
+}
+
+func (c *Compiler) emit(op code.Opcode, operands ...int) int {
+	instruction := code.Make(op, operands...)
+	position := c.addInstruction(instruction)
+	return position
+}
+
+func (c *Compiler) addInstruction(instruction []byte) int {
+	posNewInstruction := len(c.instructions)
+	c.instructions = append(c.instructions, instruction...)
+	return posNewInstruction
 }

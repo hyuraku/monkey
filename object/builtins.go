2e0a98bd0e6dfb9bdd92ba2be8fc8f6821d86ff9
hyuraku <32809703+hyuraku@users.noreply.github.com>
2025-03-25 23:09:35 +0900

    feat: refactor built-in functions and improve argument handling for 'len'

---
+++ object/builtins.go
@@ -0,0 +1,51 @@
+package object
+
+import "fmt"
+
+var Builtins = []struct {
+	Name    string
+	Builtin *Builtin
+}{
+	{
+		"len",
+		&Builtin{Fn: func(args ...Object) Object {
+			if len(args) != 1 {
+				return newError("wrong number of arguments. got=%d, want=1",
+					len(args))
+			}
+			switch arg := args[0].(type) {
+			case *Array:
+				return &Integer{Value: int64(len(arg.Elements))}
+			case *String:
+				return &Integer{Value: int64(len(arg.Value))}
+			default:
+				return newError("argument to `len` not supported, got %s",
+					args[0].Type())
+			}
+		},
+		},
+	},
+	{
+		"puts",
+		&Builtin{Fn: func(args ...Object) Object {
+			for _, arg := range args {
+				fmt.Println(arg.Inspect())
+			}
+			return nil
+		},
+		},
+	},
+}
+
+func newError(format string, a ...interface{}) *Error {
+	return &Error{Message: fmt.Sprintf(format, a...)}
+}
+
+func GetBuiltinByName(name string) *Builtin {
+	for _, bi := range Builtins {
+		if bi.Name == name {
+			return bi.Builtin
+		}
+	}
+	return nil
+}
